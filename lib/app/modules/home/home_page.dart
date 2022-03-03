b9be6c5680d25dc00eceb4e5a1280bc2e7ec0959
catalunha <catalunha.mj@gmail.com>
2022-03-03 17:24:40 -0300

    home page ui finalizada

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -5,6 +5,8 @@ import 'package:todo_list_provider/app/core/ui/theme_extensions.dart';
 import 'package:todo_list_provider/app/modules/home/widgets/home_drawer.dart';
 import 'package:todo_list_provider/app/modules/home/widgets/home_filters.dart';
 import 'package:todo_list_provider/app/modules/home/widgets/home_header.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_tasks.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_week_filter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,7 +16,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         iconTheme: IconThemeData(color: context.primaryColor),
-        backgroundColor: Colors.transparent,
+        backgroundColor: Color(0xFFFAFBFE),
         elevation: 0,
         actions: [
           PopupMenuButton(
@@ -27,7 +29,7 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      // backgroundColor: Color(0xFFFAFBFE),
+      backgroundColor: Color(0xFFFAFBFE),
       drawer: HomeDrawer(),
       body: LayoutBuilder(
         builder: (context, constraints) {
@@ -45,12 +47,19 @@ class HomePage extends StatelessWidget {
                   children: [
                     HomeHeader(),
                     HomeFilters(),
+                    HomeWeekFilter(),
+                    HomeTasks(),
                   ],
                 )),
               ),
             ),
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: context.primaryColor,
+        onPressed: () {},
+        child: Icon(Icons.add),
       ),
     );
   }
---
+++ lib/app/modules/home/widgets/task.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Task extends StatelessWidget {
+  const Task({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(color: Colors.grey),
+        ],
+      ),
+      margin: EdgeInsets.symmetric(vertical: 5),
+      child: IntrinsicHeight(
+        child: ListTile(
+          contentPadding: EdgeInsets.all(8),
+          leading: Checkbox(
+            value: true,
+            onChanged: (value) {},
+          ),
+          title: Text(
+            'Descrição da Task',
+            style: TextStyle(
+              decoration: false ? TextDecoration.lineThrough : null,
+            ),
+          ),
+          subtitle: Text(
+            '20/00/0000',
+            style: TextStyle(
+              decoration: false ? TextDecoration.lineThrough : null,
+            ),
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+            side: BorderSide(width: 1),
+          ),
+        ),
+      ),
+    );
+  }
+}
